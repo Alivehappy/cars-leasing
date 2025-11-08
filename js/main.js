423701d7ea42c30abc261604fae62702d59183c4
Alivehappy <rozhkova1809@gmail.com>
2025-11-08 15:25:44 +0300

    onlu hosting to do

--- js/main.js
+++ js/main.js
@@ -5,12 +5,18 @@ tabItem.forEach(function (element) {
 });
 function open(evt) {
   const tabTarget = evt.currentTarget;
+  const button = tabTarget.dataset.button;
   tabItem.forEach(function (item) {
     item.classList.remove("tabs__btn-item--active");
   });
   tabTarget.classList.add("tabs__btn-item--active");
+  tabContent.forEach(function (item) {
+    item.classList.remove("tabs__content-item--active");
+  });
+  document
+    .querySelector(`#${button}`)
+    .classList.add("tabs__content-item--active");
 }
-
 
 //evt.currentTarget - это элемент, на котором висит обработчик события.
 //classList - свойство, которое позволяет работать с классами элемента
